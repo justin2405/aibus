2df5998de1064e98c447fc2fe6096499258e4bc6
lyly-vuongtu <153154901+lyly-vuongtu@users.noreply.github.com>
2024-05-06 08:37:38 +0700

    6/5/2024

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -11,22 +11,7 @@ function myFunction() {
       
     }
   }
-
-let mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  
 // crate profile
 document.getElementById('createprofile').addEventListener('click', function() {
     window.location.href = "createprofile"; 
@@ -120,6 +105,8 @@ function showFile(file){
         
     }
 }
+
+
 function showImage() {
     var imageUrl = document.getElementById("image-url").value;
     
